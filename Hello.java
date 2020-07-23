9abef59bfdefa503f667ff58f7bdf8d4c8e62922
ishitagupta0807 <68687090+ishitagupta0807@users.noreply.github.com>
2020-07-23 16:05:08 +0530

    Completed this version

--- Hello.java
+++ Hello.java
@@ -3,4 +3,7 @@ public static void main(String args[]){
 System.out.println("Hello World");
   System.out.println("This is ishita");
 }
+  public void isName(){
+    System.out.println("ishita");
+  }
 }
