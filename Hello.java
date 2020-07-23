cb3a7a012cf4959e4de7c029dedac71bf28e1bc2
ishitagupta0807 <68687090+ishitagupta0807@users.noreply.github.com>
2020-07-23 16:01:29 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@ class Hello{
 public static void main(String args[]){
 System.out.println("Hello World");
   System.out.println("This is ishita");
+  
+  System.out.println("this is akash now");
+  System.out.println("I am intelligent");
 }
 }
